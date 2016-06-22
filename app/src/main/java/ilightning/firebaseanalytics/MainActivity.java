9313d05bc6d16c075e941d39ded4d4ffbf9d8eb8
iLighning <ilightning.mobileapps@gmail.com>
2016-06-22 07:27:06 +0700

    Import project

---
+++ app/src/main/java/ilightning/firebaseanalytics/MainActivity.java
@@ -0,0 +1,20 @@
+package ilightning.firebaseanalytics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Obtain the FireBase Analytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+    }
+}
